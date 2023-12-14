2fa3de38b2fcefa9d76c9717e4523d259afc40b1
vishruth <vishruth555@gmail.com>
2023-12-14 13:13:47 +0530

    added medical history

--- lib/alert/profile_details.dart
+++ lib/alert/profile_details.dart
@@ -10,6 +10,52 @@ class Profile extends StatelessWidget {
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30, 25, 30, 0),
+            child: Container(
+              height: 100,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: const Offset(0, 5),
+                        color: Colors.black.withOpacity(.2),
+                        spreadRadius: 2,
+                        blurRadius: 10)
+                  ]),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        Icons.account_circle,
+                        color: Colors.white,
+                        size: 40,
+                      )),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        'Batman',
+                        style: TextStyle(color: Colors.black, fontSize: 30),
+                      ),
+                      Text(
+                        'uid:98562387236',
+                        style: TextStyle(
+                            color: Colors.grey.shade700, fontSize: 14),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
           Container(
             child: Container(
               padding: const EdgeInsets.symmetric(horizontal: 30),
@@ -39,6 +85,56 @@ class Profile extends StatelessWidget {
                   itemDashboard('Vehicle No', 'KA 03 FF 1234',
                       Icons.car_crash_outlined, Colors.pinkAccent, 18),
                 ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30, 25, 30, 0),
+            child: Container(
+              height: 250,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: const Offset(0, 5),
+                        color: Colors.black.withOpacity(.2),
+                        spreadRadius: 2,
+                        blurRadius: 10)
+                  ]),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(
+                          Icons.medical_information,
+                          color: Colors.white,
+                          size: 35,
+                        )),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Medical history',
+                      style: TextStyle(color: Colors.black, fontSize: 25),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                      style:
+                          TextStyle(color: Colors.grey.shade700, fontSize: 14),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
